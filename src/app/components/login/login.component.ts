90d37dad29e4610a83d690d02828cc78d887588c
mohammedhaggagg <mohammedhaggagg@gmial.com>
2025-03-20 15:37:39 +0200

    Adding login and registration forms

--- src/app/components/login/login.component.ts
+++ src/app/components/login/login.component.ts
@@ -1,11 +1,19 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './login.component.html',
-  styleUrl: './login.component.css'
+  styleUrl: './login.component.css',
 })
 export class LoginComponent {
-
+  formValues ={
+    email: '',
+    Password: ''  
+  }
+  handleSubmitForm(form: any) {
+    console.log(form);
+    console.log(form.value);
+  }
 }
--- src/app/components/register/register.component.ts
+++ src/app/components/register/register.component.ts
@@ -1,11 +1,69 @@
 import { Component } from '@angular/core';
+import {
+  AbstractControl,
+  FormControl,
+  FormGroup,
+  ReactiveFormsModule,
+  Validators,
+} from '@angular/forms';
 
 @Component({
   selector: 'app-register',
-  imports: [],
+  imports: [ReactiveFormsModule],
   templateUrl: './register.component.html',
-  styleUrl: './register.component.css'
+  styleUrl: './register.component.css',
 })
 export class RegisterComponent {
+  registerForm: FormGroup;
 
+  // constructor() {
+  //   this.registerForm = new FormGroup({
+  //     name :new FormControl('',[Validators.required,Validators.minLength(3),Validators.maxLength(100)]),
+  //     email :new FormControl('',[Validators.email,Validators.required]),
+  //     userName:new FormControl('',[Validators.required,Validators.minLength(3),Validators.maxLength(100),Validators.pattern(/^\S+$/)]),
+  //     password :new FormControl('',[Validators.required,Validators.minLength(8),Validators.pattern(/^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@*$%#])[A-Za-z\d@*$%#]+$/),]),
+
+  //   });
+
+  // }
+  constructor() {
+    this.registerForm = new FormGroup(
+      {
+        name: new FormControl('', [
+          Validators.required,
+          Validators.minLength(3),
+          Validators.maxLength(100),
+        ]),
+        email: new FormControl('', [Validators.required, Validators.email]),
+        userName: new FormControl('', [
+          Validators.required,
+          Validators.minLength(3),
+          Validators.maxLength(100),
+          Validators.pattern(/^\S+$/),
+        ]),
+        password: new FormControl('', [
+          Validators.required,
+          Validators.minLength(8),
+          Validators.pattern(
+            /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@*$%#])[A-Za-z\d@*$%#]+$/
+          ),
+        ]),
+        confirmPassword: new FormControl('', [Validators.required]),
+      },
+      this.matchPasswords
+    );
+  }
+  // Custom validator to check if passwords match
+  matchPasswords(control: AbstractControl) {
+    const formGroup = control as FormGroup;
+    const password = formGroup.get('password')?.value;
+    const confirmPassword = formGroup.get('confirmPassword')?.value;
+    return password === confirmPassword ? null : { mismatch: true };
+  }
+  get formControls() {
+    return this.registerForm.controls;
+  }
+  handleSubmitForm() {
+    console.log(this.registerForm);
+  }
 }
